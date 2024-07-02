db4ced47ee23c46df999b0ce2e5da994678c0f4b
pradeepkudukkil <88960052+pradeepkudukkil@users.noreply.github.com>
2024-07-02 09:48:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "my_ec2" {
   ami           = "ami-0c55b159cbfafe1f0"  
   instance_type = "t2.micro"
 
   tags = {
-    Name = "example-instance"
+    Name = "my-instance"
   }
 }
