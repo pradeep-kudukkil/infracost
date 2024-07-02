7c8821d2b6b5560e2d1c27d088c67ea919b9b568
pradeepkudukkil <88960052+pradeepkudukkil@users.noreply.github.com>
2024-07-02 10:01:47 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,9 +1,5 @@
 resource "aws_db_instance" "my_rds" {
-  identifier            = "my-db-instance"
-  allocated_storage     = 20
-  storage_type          = "gp2"
   engine                = "mysql"
-  engine_version        = "5.7"
   instance_class        = "db.t2.micro"
   name                  = "mydatabase"
   username              = "admin"
